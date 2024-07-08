45c880b48018c61c0f9506b0ab7c7aa3d3279fa3
ashar aial <bashar@etmaam.com.sa>
2024-07-08 09:42:18 +0300

    Design Blade using Tailwind and apply Techniques laravel

---
+++ resources/views/components/job-card-wide.blade.php
@@ -0,0 +1,21 @@
+<x-panel class="flex  gap-x-6">
+    <div>
+    <x-employer-logo/>
+
+    </div>
+
+    <div class="flex-1 flex  flex-col">
+        <a href="#" class="self-start text-sm text-gray-400">Laracast</a>
+        <h3 class="font-bold text-xl mt-3 group-hover:text-blue-800 transition-colors duration-500">Video Producer</h3>
+        <p class="text-sm text-gray-400 mt-auto  mt-4">Full Time - $60,000</p>
+    </div>
+
+    <div>
+        <x-tag>Tag 1</x-tag>
+        <x-tag>Tag 2</x-tag>
+        <x-tag>Tag 3</x-tag>
+
+    </div>
+
+
+</x-panel>
---
+++ resources/views/components/job-card.blade.php
@@ -0,0 +1,25 @@
+<x-panel class="flex flex-col text-center">
+    <div class="self-start text-sm">Laracast</div>
+
+
+    <div  class="py-8">
+        <h3 class="group-hover:text-blue-800 text-xl font-bold transition-colors duration-500">Video Producer</h3>
+        <p class="text-sm mt-4">Full Time - $60,000</p>
+    </div>
+
+
+    <div class="flex justify-between items-center mt-auto">
+        <div>
+        <x-tag size="small">Backend</x-tag>
+        <x-tag size="small">FrontEnd</x-tag>
+        <x-tag size="small">API</x-tag>
+
+        </div>
+
+
+
+        <x-employer-logo :width="42" />
+    </div>
+
+
+</x-panel>
